db8c9636ff160ee5369cdebc3a4fb433ce6999ea
Kevin Boyle <papershack@gmail.com>
2019-07-25 23:15:37 -0700

    set up basic boilerplate for unit/integration testing an Express app

---
+++ test/app.test.js
@@ -0,0 +1,12 @@
+const app = require('../app.js')
+const expect = require('chai').expect
+const request = require('supertest')
+
+describe('Express App', () => {
+    it('GET / should return a message', () => {
+       return request(app)
+            .get('/')
+            .expect(200, 'Hello Express!')
+    })
+})
+
